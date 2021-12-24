360f5206dfb6aac7b1971ca9aa90841ba6ab1e4c
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2021-12-24 16:04:31 +0530

    Initial form validation code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterformComponent } from './registerform/registerform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registerform/registerform.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registerform',
+  templateUrl: './registerform.component.html',
+  styleUrls: ['./registerform.component.scss']
+})
+export class RegisterformComponent implements OnInit {
+
+  constructor() { }
+  registerForm: any; // form
+
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      "firstName": new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      "lastName": new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      "emailId": new FormControl(null, [Validators.required, Validators.email]),
+      "mobileNumber": new FormControl(null, [Validators.required, Validators.pattern('[0-9]*')])
+    });
+  }
+
+  submitData() {
+    console.log(this.registerForm.value);
+    
+  }
+
+  get firstname() { return this.registerForm.get('firstName'); }
+  get lastname() { return this.registerForm.get('lastName'); }
+  get emailid() { return this.registerForm.get('emailId'); }
+  get mobilenumber() { return this.registerForm.get('mobileNumber'); }
+
+}
